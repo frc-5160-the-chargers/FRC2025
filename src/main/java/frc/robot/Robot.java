7f25133627bcfae9360bb32e83153f73f182df90
Daniel Chen <Daniel_Chen@caryacademy.org>
2024-11-03 08:20:00 -0500

    Added deps and removed kotlin

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+    public Robot() {
+        System.out.println("Hello world!");
+    }
+}
