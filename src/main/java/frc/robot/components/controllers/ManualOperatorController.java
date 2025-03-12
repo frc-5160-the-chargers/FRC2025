8359604e5b15e56e5075484c5f9ea665c4b843db
Daniel Chen <Daniel_Chen@caryacademy.org>
2025-03-12 09:04:26 -0400

    Moved InputStreams to subclasses

---
+++ src/main/java/frc/robot/components/controllers/ManualOperatorController.java
@@ -0,0 +1,47 @@
+package frc.robot.components.controllers;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.chargers.utils.InputStream;
+
+import static frc.robot.constants.OtherConstants.IS_DANIEL_COMPUTER;
+
+@Logged
+public class ManualOperatorController extends CommandXboxController implements Subsystem {
+	private static final int PORT = IS_DANIEL_COMPUTER ? 0 : 1;
+	
+	public ManualOperatorController() {
+		super(PORT);
+		this.register();
+	}
+	
+	public Command rumbleCmd(RumbleType type, double time) {
+		return this.runOnce(() -> setRumble(type, 0.5))
+			       .andThen(
+				       Commands.waitSeconds(time),
+				       this.runOnce(() -> setRumble(type, 0.5))
+			       )
+			       .withName("operator controller rumble");
+	}
+	
+	public final InputStream manualElevatorInput =
+		InputStream.of(this::getLeftY)
+			.deadband(0.1, 1)
+			.times(-0.5);
+	public final InputStream manualPivotInput =
+		InputStream.of(this::getRightY)
+			.deadband(0.1, 1)
+			.times(-0.3);
+	public final InputStream climbUpInput =
+		InputStream.of(this::getRightTriggerAxis)
+			.deadband(0.1, 1)
+			.times(0.5);
+	public final InputStream climbDownInput =
+		InputStream.of(this::getLeftTriggerAxis)
+			.deadband(0.1, 1)
+			.times(0.5);
+}
