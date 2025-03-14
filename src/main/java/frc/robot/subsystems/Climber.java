13cd58088ac9bf98e93509fab78f6038ce57061d
Daniel Chen <Daniel_Chen@caryacademy.org>
2025-03-14 18:34:32 -0400

    Better gravity comp on Climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,6 +5,7 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.MomentOfInertia;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -23,10 +24,12 @@ import static edu.wpi.first.units.Units.*;
 import static frc.chargers.utils.UtilMethods.tryUntilOk;
 import static frc.chargers.utils.UtilMethods.waitThenRun;
 
+// Convention: + is forward, - is backward
 public class Climber extends StandardSubsystem {
-	private static final TunableNum KG_VOLTS = new TunableNum("climber/kG", -0.4);
+	private static final TunableNum KG_VOLTS = new TunableNum("climber/kG", 0);
 	private static final TunableNum KG_START_LIMIT_DEG = new TunableNum("climber/kGStartLimitDeg", 90);
 	
+	private static final Angle START_ANGLE = Degrees.of(-80);
 	private static final double GEAR_RATIO = 75.0;
 	private static final MomentOfInertia MOI = KilogramSquareMeters.of(.003);
 	private static final int LEADER_ID = 14;
@@ -41,9 +44,10 @@ public class Climber extends StandardSubsystem {
 	private final SparkMax follower = new SparkMax(FOLLOWER_ID, kBrushless);
 	@Logged private final DigitalInput limitSwitch = new DigitalInput(2);
 	@Logged private double voltageReq = 0;
+	@Logged private boolean shouldApplyKg = false;
 	
 	public Climber() {
-		waitThenRun(2, () -> motor.encoder().setPositionReading(Degrees.zero()));
+		waitThenRun(2, () -> motor.encoder().setPositionReading(START_ANGLE));
 		motor.setControlsConfig(Motor.ControlsConfig.EMPTY.withGearRatio(GEAR_RATIO));
 		tryUntilOk(follower, () -> follower.configure(CONFIG.follow(LEADER_ID, true), kResetSafeParameters, kPersistParameters));
 	}
@@ -51,14 +55,25 @@ public class Climber extends StandardSubsystem {
 	public Command setPowerCmd(InputStream output) {
 		return this.run(() -> {
 			voltageReq = output.get() * 12;
-		    if (voltageReq < 0 && limitSwitch.get()) voltageReq = 0;
-			motor.setVoltage(voltageReq);
+			if (limitSwitch.get() && voltageReq < 0) {
+				requestStop();
+			} else {
+				voltageReq += getKgVolts();
+				motor.setVoltage(voltageReq);
+			}
 		})
 	       .withName("set voltage(climber)");
 	}
 	
+	@Logged
+	public double getKgVolts() {
+		double angleRads = motor.encoder().positionRad();
+		shouldApplyKg = angleRads > degreesToRadians(KG_START_LIMIT_DEG.get());
+		return shouldApplyKg ? KG_VOLTS.get() * Math.cos(angleRads) : 0;
+	}
+	
 	@Override
 	protected void requestStop() {
-		motor.setVoltage(0);
+		motor.setVoltage(getKgVolts());
 	}
 }
